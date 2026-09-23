496bbb7f2e5be632774c603cb65d7b4c3afbf12d
Ananya-220 <ananya@webexpertinfotech.com>
2026-09-23 14:36:32 +0530

    new concepts in js

--- index.js
+++ index.js
@@ -97,3 +97,186 @@ function attachEventListeners() {
         })
 }
 attachEventListeners();
+
+
+
+// Ep. 18
+const radius = [3 , 1 , 2 , 4];
+
+const calculateArea = function (radius) {
+    const output = [];
+    for (let i = 0 ; i < radius.length ; i++) {
+        output.push(Math.PI * radius[i] * radius[i]);
+    }
+    return output;
+};
+
+console.log(calculateArea(radius));
+
+const calculateCircumference = function (radius) {
+    const output = [];
+    for (let i = 0 ; i < radius.length ; i++) {
+        output.push(2 * Math.PI * radius[i]);
+    }
+    return output;
+}; 
+
+console.log(calculateCircumference(radius));
+
+const calculateDiameter = function (radius) {
+    const output = [];
+    for (let i = 0 ; i < radius.length ; i++) {
+        output.push(2 * radius[i]);
+    }
+    return output;
+}; 
+
+console.log(calculateDiameter(radius)); 
+
+// More optimised code
+const radius = [3 , 1 , 2 , 4];
+
+const area = function (radius) {
+    return Math.PI * radius * radius;
+};
+
+const circumference = function (radius) {
+    return 2 * Math.PI * radius;
+};
+
+const diameter = function (radius) {
+    return 2 * radius;
+};
+
+const calculate = function (radius , logic) {
+    const output = [];
+    for (let i = 0 ; i < radius.length ; i++) {
+        output.push(logic(radius[i]));
+    }
+    return output;
+}
+
+console.log(calculate(radius , area));
+console.log(calculate(radius , circumference));
+console.log(calculate(radius , diameter));
+
+
+// EP.19
+// 1. Map Function - used to transform an array.
+const arr = [5 , 1 , 3 , 2 , 6];
+
+// Double of the array values
+function double(x) {
+    return x * 2;
+}
+const output = arr.map(double);
+console.log(output);
+
+// Triple of the array values
+function triple(x) {
+    return x * 3;
+}
+const output = arr.map(triple);
+console.log(output);
+
+// Binary of the array values
+function binary(x) {
+    return x.toString(2);
+}
+const output = arr.map(binary);
+console.log(output); 
+
+// 2. Filter Function - used to filter the values inside the array.
+const arr = [5 , 1 , 3 , 2 , 6];
+
+// Filter odd values
+function isOdd(x) {
+    return x % 2;
+}
+const output = arr.filter(isOdd);
+console.log(output);
+
+// Filter odd values
+function isEven(x) {
+    return x % 2 == 0;
+}
+const output = arr.filter(isEven);
+console.log(output);
+
+// Filter Greater than 4 values
+function greaterThan4(x) {
+    return x > 4;
+}
+const output = arr.filter(greaterThan4);
+console.log(output);
+
+
+// 3. Reduce Function - takes all elements of an array and outputs a single value
+const arr = [5 , 1 , 3 , 2 , 6];
+
+// Sum (Normal Method)
+function findSum(arr) {
+    let sum = 0;
+    for (let i = 0 ; i < arr.length ; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+console.log(findSum(arr));
+
+// Using Reduce Function
+const output = arr.reduce(function(add , curr) {
+    add = add + curr;
+    return add;
+} , 0);
+console.log(output);
+
+
+// Max (Normal Method)
+function findMax(arr) {
+    let max = 0;
+    for (let i = 0 ; i < arr.length ; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+console.log(findMax(arr));
+
+// Using Reduce Function
+const output = arr.reduce(function (max , curr) {
+    if (curr > max) {
+        max = curr;
+    }
+    return max;
+} , 0);
+console.log(output);
+
+// Examples of these 3 functions (map , filter , reduce)
+const users = [
+    { firstName : "Ananya" , lastName : "Sharma" , age : 20 } , 
+    { firstName : "Donald" , lastName : "Trump" , age : 75 } ,
+    { firstName : "Elon" , lastName : "Musk" , age : 50 } ,
+    { firstName : "Deepika" , lastName : "Padukone" , age : 20 } ,
+];
+
+// List of full names
+const output = users.map(x => x.firstName + " " + x.lastName);
+console.log(output);
+
+// List of age 
+const output1 = users.reduce(function (acc , curr) {
+    if (acc[curr.age]) {
+        acc[curr.age] = ++acc[curr.age];
+    }
+    else {
+        acc[curr.age] = 1;
+    }
+    return acc;
+} , {});
+console.log(output1);
+
+// Names of people whose age is less than 30
+const output2 = users.filter(x => x.age < 30).map((x) => x.firstName);
+console.log(output2);
